76d7ec59cae77897a225e9bbc50ed5006aafd0b0
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-02-21 15:32:40 -0800

    fix(ui): Support MenuItem as non-anchors

--- src/sentry/static/sentry/app/components/menuItem.tsx
+++ src/sentry/static/sentry/app/components/menuItem.tsx
@@ -56,15 +56,29 @@ class MenuItem extends React.Component<Props> {
         </Link>
       );
     }
+    if (this.props.href) {
+      return (
+        <a
+          title={this.props.title}
+          onClick={this.handleClick}
+          href={this.props.href}
+          tabIndex={-1}
+        >
+          {this.props.children}
+        </a>
+      );
+    }
+
     return (
-      <a
+      <span
+        className="menu-target"
+        role="button"
         title={this.props.title}
         onClick={this.handleClick}
-        href={this.props.href}
         tabIndex={-1}
       >
         {this.props.children}
-      </a>
+      </span>
     );
   };
 
